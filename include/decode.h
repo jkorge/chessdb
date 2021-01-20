3af4175db0a0a93cfd3b9e14687400d6c424ae41
James Korge <james@korgetech.com>
2021-01-20 08:28:07 -0500

    Updated decoder/encoder to match bitboard process

--- include/decode.h
+++ include/decode.h
@@ -1,202 +1,195 @@
 #ifndef DECODE_H
 #define DECODE_H
 
-#include <iostream>
-#include <utility>
-
 #include "types.h"
-#include "util.h"
-
-static const ply blank_ply;
+#include "board.h"
 
 class Decoder{
-private:
-    static ply cply;
-    static pname name;
-    static int action[2], castle;
-    static bool capture, check, mate, queen_rook;
-    static ptype promo;
+public:    
+    ply decode_ply(eply, ChessBoard&);
 
-    void decode_name(BYTE);
-    void decode_queen_rookmove(BYTE);
-    void decode_pawn_promotion(BYTE);
+    ptype decode_type(BYTE);
 
-    void decode_check(BYTE);
-    void decode_mate(BYTE);
-    void decode_castle(BYTE);
+    color decode_color(pname);
 
-    void pawn_ply(BYTE);
-    void knight_ply(BYTE);
-    void bishop_ply(BYTE);
-    void rook_ply(BYTE);
-    void queen_ply(BYTE, bool);
-    void king_ply(BYTE);
+    pname decode_name(BYTE);
 
-    void reset();
-    void set();
+    bool decode_capture(BYTE);
 
-public:
-    Decoder();
-    
-    void decode_ply(eply, ply*);
+    bool decode_check(BYTE);
 
-    void decode_name(BYTE, pname*);
-    void decode_queen_rookmove(BYTE, bool*);
-    void decode_pawn_promotion(BYTE, ptype*);
+    bool decode_mate(BYTE);
 
-    void decode_check(BYTE, bool*);
-    void decode_mate(BYTE, bool*);
-    void decode_castle(BYTE, int*);
+    ptype decode_pawn_promotion(BYTE);
 
-    void pawn_ply(BYTE, int*);
-    void knight_ply(BYTE, int*);
-    void bishop_ply(BYTE, int*);
-    void rook_ply(BYTE, int*);
-    void queen_ply(BYTE, int*, bool=false);
-    void king_ply(BYTE, int*, int*);
+    ptype decode_queen_axis(BYTE);
+
+    coords<int> pawn_action(BYTE, color);
+
+    coords<int> knight_action(BYTE);
+
+    coords<int> bishop_action(BYTE);
+
+    coords<int> rook_action(BYTE);
+
+    coords<int> queen_action(BYTE, ptype);
+
+    coords<int> king_action(BYTE);
 };
 
-ply Decoder::cply;
-bool Decoder::capture, Decoder::check, Decoder::mate, Decoder::queen_rook;
-ptype Decoder::promo;
-int Decoder::action[2], Decoder::castle;
-pname Decoder::name;
+ply Decoder::decode_ply(eply e, ChessBoard& board){
+    ply p;
 
-Decoder::Decoder(){ this->reset(); }
+    p.name = this->decode_name(e.piece);
+    p.type = this->decode_type(e.piece);
+    if(p.type == king && (p.name >= 8 & p.name <= 23)){ p.type = queen; }
+    p.c = this->decode_color(p.name);
 
-void Decoder::reset(){
-    this->cply = blank_ply;
-    this->name = NONAME;
-    for(int i=0;i<2;i++){ this->action[i] = -1; }
-    this->castle = 0;
-    this->capture = false;
-    this->check = false;
-    this->mate = false;
-    this->queen_rook = false;
-    this->promo = pawn;
-}
+    p.capture = this->decode_capture(e.action);
+    p.check = this->decode_check(e.action);
+    p.mate = this->decode_mate(e.action);
 
-void Decoder::set(){
-    this->cply = ply{
-        pce{this->name},
-        this->action[0],
-        this->action[1],
-        this->castle,
-        this->capture,
-        this->check,
-        this->mate,
-        this->promo
-    };
-}
-
-void Decoder::decode_ply(eply e, ply *dst){
-    this->reset();
-    this->decode_name(e.name);
-    switch(util::name2type(this->name)){
-        case king: this->king_ply(e.action); break;
-        case queen: this->decode_queen_rookmove(e.name); this->queen_ply(e.action, this->queen_rook); break;
-        case rook: this->rook_ply(e.action); break;
-        case bishop: this->bishop_ply(e.action); break;
-        case knight: this->knight_ply(e.action); break;
-        case pawn: this->decode_pawn_promotion(e.name); this->pawn_ply(e.action); break;
+    if(p.type == pawn){ p.promo = this->decode_pawn_promotion(e.action); }
+    else              { p.promo = pawn; }
+    
+    coords<int> src = rf(board.mat[p.name]),
+                dst,
+                delta;
+    switch(p.type){
+        case pawn:   delta = this->pawn_action(e.action, p.c); break;
+        case knight: delta = this->knight_action(e.action); break;
+        case bishop: delta = this->bishop_action(e.action); break;
+        case rook:   delta = this->rook_action(e.action); break;
+        case queen:  delta = this->queen_action(e.action, this->decode_queen_axis(e.piece)); break;
+        default:     delta = this->king_action(e.action);
     }
-    this->set();
-    *dst = this->cply;
+    dst = src + delta;
+    p.src = mask(src);
+    p.dst = mask(dst);
+
+    if(p.type == king){
+        switch(delta[1]){
+            case  2: p.castle =  1; break;
+            case -2: p.castle = -1; break;
+            default: p.castle =  0;
+        }
+    }
+
+    return p;
 }
 
-/*
-    PRIVATE FUNCS - MODIFY MEMBERS
-*/
-
-void Decoder::decode_name(BYTE name){ this->decode_name(name, &(this->name)); }
-
-void Decoder::decode_queen_rookmove(BYTE name){ this->decode_queen_rookmove(name, &(this->queen_rook)); }
-
-void Decoder::decode_pawn_promotion(BYTE name){ this->decode_pawn_promotion(name, &(this->promo)); }
-
-void Decoder::decode_check(BYTE act){ this->decode_check(act, &(this->check)); }
-
-void Decoder::decode_mate(BYTE act){ this->decode_mate(act, &(this->mate)); }
-
-void Decoder::decode_castle(BYTE act){ this->decode_castle(act, &(this->castle)); }
-
-void Decoder::pawn_ply(BYTE act){ this->pawn_ply(act, this->action); }
-
-void Decoder::knight_ply(BYTE act){ this->knight_ply(act, this->action); }
-
-void Decoder::bishop_ply(BYTE act){ this->bishop_ply(act, this->action); }
-
-void Decoder::rook_ply(BYTE act){ this->rook_ply(act, this->action); }
-
-void Decoder::queen_ply(BYTE act, bool qr){ this->queen_ply(act, this->action, qr); }
-
-void Decoder::king_ply(BYTE act){ this->king_ply(act, this->action, &(this->castle)); }
-
-
-/*
-    PUBLIC FUNCS - MODIFY EXTERNAL ADDRESSES
-*/
-
-void Decoder::decode_name(BYTE name, pname *ply_name){ *ply_name = static_cast<pname>(name & ~(7<<5)); }
-
-void Decoder::decode_queen_rookmove(BYTE name, bool *qr){ *qr = 1<<5 & name; }
-
-void Decoder::decode_pawn_promotion(BYTE name, ptype *promo){ *promo = static_cast<ptype>((224 & name)>>5); }
-
-void Decoder::decode_check(BYTE act, bool *chk){ *chk = (bool) act>>6; }
-
-void Decoder::decode_mate(BYTE act, bool *mte){ *mte = (bool) act>>7; }
-
-void Decoder::decode_castle(BYTE act, int *cas){ *cas = (act & 12) == 12 ? 1 - (2 * (act>>4)) : 0; }
-
-void Decoder::pawn_ply(BYTE act, int *dst){
-    // 0xpppcmm
-    bool cap = act & (1<<5);
-    int mov = 7 & act;
-
-    dst[0] = ((act & 4)>>2 ? -1 : 1) * ((act & 3)==3 ? 2 : 1);
-    dst[1] = !cap ? 0 : ((act & 1) ? 1 : -1);
+ptype Decoder::decode_type(BYTE _piece){
+    switch(static_cast<int>(_piece & 31)){
+        case  0:
+        case  7:
+        case 24:
+        case 31: return rook;
+        case  1:
+        case  6:
+        case 25:
+        case 30: return knight;
+        case  2:
+        case  5:
+        case 26:
+        case 29: return bishop;
+        case  3:
+        case 27: return queen;
+        case  4:
+        case 28: return king;
+        case  8:
+        case  9:
+        case 10:
+        case 11:
+        case 12:
+        case 13:
+        case 14:
+        case 15:
+        case 16:
+        case 17:
+        case 18:
+        case 19:
+        case 20:
+        case 21:
+        case 22:
+        case 23: return static_cast<ptype>((_piece & 224) >> 5);
+        default: return NOTYPE;
+    }
 }
 
-void Decoder::knight_ply(BYTE act, int *dst){
-    // 00000odt
-    int step = 1 - 2 * ((act & 4)>>2);
-    int skip = 2 - 4 * (act & 1);
-    dst[0] = (act & 2) ? step : skip;
-    dst[1] = (act & 2) ? skip : step;
+
+color Decoder::decode_color(pname name){ return name == NONAME ? NOCOLOR : (name > 15 ? black : white); }
+
+pname Decoder::decode_name(BYTE _piece){ return static_cast<pname>(_piece & 31); }
+
+bool Decoder::decode_capture(BYTE _action){ return CAPTURE_ & _action; }
+
+bool Decoder::decode_check(BYTE _action){ return CHECK_ & _action; }
+
+bool Decoder::decode_mate(BYTE _action){ return MATE_ & _action; }
+
+ptype Decoder::decode_pawn_promotion(BYTE _action){ return static_cast<ptype>((_action & 28) >> 2); }
+
+ptype Decoder::decode_queen_axis(BYTE _piece){ return (QAROOK_ & _piece) ? rook : bishop; }
+
+coords<int> Decoder::pawn_action(BYTE _action, color c){
+    // 000ppp<rr/ff>
+    bool cap = _action & 32,
+         m = _action & 2;
+
+    int dr = c * (cap ? 1 : (m ? 2 : 1)),
+        df = cap ? (m ? 1 : -1) : 0;
+
+    return {dr, df};
 }
 
-void Decoder::bishop_ply(BYTE act, int *dst){
+coords<int> Decoder::knight_action(BYTE _action){
+    // 00000aot
+    int o = 1 - (2 * (bool)(_action & 2)),
+        t = 2 - (4 * (bool)(_action & 1));
+    if(_action & 4){ return {o, t}; }
+    else           { return {t, o}; }
+}
+
+coords<int> Decoder::bishop_action(BYTE _action){
     // 000admmm
-    int axis = 1 - (2 * (act & 16)>>4);
-    int dir = 1 - (2 * (act & 8)>>3);
-    int mag = act & 7;
-    dst[0] = dir * mag;
-    dst[1] = axis * dst[0];
+    bool d = _action & 8,
+         a = _action & 16;
+    int  m = _action & 7;
+    if(d ^ a){
+        if(d){ return {-m, -m}; }
+        else { return {m, -m}; }
+    }
+    else{
+        if(d){ return {-m, m}; }
+        else { return {m, m}; }
+    }
 }
 
-void Decoder::rook_ply(BYTE act, int *dst){
+coords<int> Decoder::rook_action(BYTE _action){
     // 000admmm
-    bool axis = act & 16;
-    int dir = 1 - (2 * (act & 8)>>3);
-    int mag = act & 7;
-    dst[0] = axis ? dir * mag : 0;
-    dst[1] = axis ? 0 : dir * mag;
+    int val = (1 - (2 * ((_action & 8) >> 3))) * (_action & 7);
+    if(_action & 16){ return {val, 0}; }
+    else            { return {0, val}; }
 }
 
-void Decoder::queen_ply(BYTE act, int *dst, bool qr){
+coords<int> Decoder::queen_action(BYTE _action, ptype pt){
     // 000admmm
-    if(qr){ this->rook_ply(act, dst); }
-    else{ this->bishop_ply(act, dst); }
+    switch(pt){
+        case rook: return this->rook_action(_action);
+        default:   return this->bishop_action(_action);
+    }
 }
 
-void Decoder::king_ply(BYTE act, int *dst, int *cas){
+coords<int> Decoder::king_action(BYTE _action){
     // 000cffrr
-    int radj = (act & 3);
-    int fadj = (act & 12)>>2;
-    this->decode_castle(act, cas);
-    dst[0] = radj - (3 * (radj>>1));
-    dst[1] = *cas ? 2 * *cas : fadj - (3 * fadj>>1);
+    int dr = _action & 3,
+        df = (_action & 12) >> 2;
+
+    if(dr == 2){ dr = -1; }
+    if(df == 2){ df = -1; }
+    if(_action & 16){ df *= 2; }
+    return {dr, df};
+
 }
 
 #endif
--- include/encode.h
+++ include/encode.h
@@ -1,245 +1,144 @@
 #ifndef ENCODE_H
 #define ENCODE_H
 
-#include <utility>
-#include <stdlib.h>
-#include <cctype>
-#include <vector>
-#include <unordered_set>
-
 #include "types.h"
-#include "util.h"
-
-static const eply blank_eply;   // name and action default to 0
 
 class Encoder {
-private:
-    static eply cply;
-    static BYTE name, action;
-
-    void encode_name(pname);
-    void encode_queen_rookmove(int*, int*);
-    void encode_pawn_promotion(ptype);
-
-    void encode_capture(bool);
-    void encode_check(bool);
-    void encode_mate(bool);
-    
-    void pawn_ply(int*, int*);
-    void knight_ply(int*, int*);
-    void bishop_ply(int*, int*);
-    void rook_ply(int*, int*);
-    void queen_ply(int*, int*);
-    void king_ply(int*, int*);
-
-    void reset();
-    void set();
-
 public:
-    Encoder();
 
-    void encode_game(game, egame*);
-    void encode_turn(turn, eturn*);
-    void encode_ply(ply, eply*);
+    eply encode_ply(const ply&);
 
-    // Overloads of private funcs - requires address(es) to store results
-    void encode_name(pname, BYTE*);
-    void encode_queen_rookmove(int*, int*, BYTE*);
-    void encode_pawn_promotion(ptype, BYTE*);
+    BYTE encode_piece(const ply&);
 
-    void encode_capture(bool, BYTE*);
-    void encode_check(bool, BYTE*);
-    void encode_mate(bool, BYTE*);
+    BYTE encode_action(const ply&);
 
-    void pawn_ply(int*, int*, BYTE*, bool=false);
-    void knight_ply(int*, int*, BYTE*);
-    void bishop_ply(int*, int*, BYTE*);
-    void rook_ply(int*, int*, BYTE*);
-    void queen_ply(int*, int*, BYTE*);
-    void king_ply(int*, int*, BYTE*);
+    BYTE encode_mat(ptype, color);
 
-    void encode_header(std::vector<pgndict>&, std::map<int, str>&);
+    BYTE encode_capture(bool);
+
+    BYTE encode_check(bool);
+
+    BYTE encode_mate(bool);
+
+    BYTE encode_pawn_promotion(ptype, bool=true);
+
+    template<typename T>
+    BYTE encode_queen_axis(const coords<T>&, const coords<T>&);
+
+    template<typename T>
+    BYTE pawn_action(const coords<T>&, const coords<T>&, bool, ptype);
+
+    template<typename T>
+    BYTE knight_action(const coords<T>&, const coords<T>&);
+
+    template<typename T>
+    BYTE bishop_action(const coords<T>&, const coords<T>&);
+
+    template<typename T>
+    BYTE rook_action(const coords<T>&, const coords<T>&);
+
+    template<typename T>
+    BYTE queen_action(const coords<T>&, const coords<T>&);
+
+    template<typename T>
+    BYTE king_action(const coords<T>&, const coords<T>&);
 };
 
-eply Encoder::cply;
-BYTE Encoder::name, Encoder::action;
+eply Encoder::encode_ply(const ply& p){ return eply(this->encode_piece(p), this->encode_action(p)); }
 
-Encoder::Encoder(){ this->reset(); }
-
-void Encoder::reset(){
-    this->cply = blank_eply;
-    this->name = 0;
-    this->action = 0;
+BYTE Encoder::encode_piece(const ply& p){
+    BYTE _piece = static_cast<BYTE>(p.name);
+    // if(p.type == pawn && p.promo){ _piece |= this->encode_pawn_promotion(p.promo); }
+    if(p.type != pawn && (p.name >= 8 & p.name <= 23)){ _piece |= this->encode_pawn_promotion(p.type); }
+    if(p.type == queen)                               { _piece |= this->encode_queen_axis(rf(p.src), rf(p.dst)); }
+    return _piece;
 }
 
-void Encoder::set(){
-    this->cply = eply{
-        this->name,
-        this->action
-    };
-}
+BYTE Encoder::encode_action(const ply& p){
+    coords<int> src = rf(p.src),
+                dst = rf(p.dst);
 
-void Encoder::encode_game(game g, egame *dst){
-    for(int i=0; i<g.turns.size(); i++){
-        eturn et;
-        this->encode_turn(g[i], &et);
-        (*dst).append(et);
+    BYTE _action = this->encode_capture(p.capture) | this->encode_check(p.check) | this->encode_mate(p.mate);
+    switch(p.type){
+        case pawn:   _action |= this->pawn_action(src, dst, p.capture, p.promo); break;
+        case knight: _action |= this->knight_action(src, dst);                   break;
+        case bishop: _action |= this->bishop_action(src, dst);                   break;
+        case rook:   _action |= this->rook_action(src, dst);                     break;
+        case queen:  _action |= this->queen_action(src, dst);                    break;
+        default:     _action |= this->king_action(src, dst);
     }
+    return _action;
 }
 
-void Encoder::encode_turn(turn t, eturn *dst){
-    this->encode_ply(t.white, &(dst->white));
-    if(t.black.piece.name != NONAME){ this->encode_ply(t.black, &(dst->black)); }
-}
+BYTE Encoder::encode_capture(bool cap){ return cap ? CAPTURE_ : 0; }
 
-void Encoder::encode_ply(ply p, eply *dst){
-    this->reset();
+BYTE Encoder::encode_check(bool chk){ return chk ? CHECK_ : 0; }
 
-    // Piece name
-    this->encode_name(p.piece.name);
+BYTE Encoder::encode_mate(bool mte){ return mte ? MATE_ : 0; }
 
-    // Capture, check, and mate flags
-    this->encode_capture(p.capture);
-    this->encode_check(p.check);
-    this->encode_mate(p.mate);
+BYTE Encoder::encode_pawn_promotion(ptype promo, bool pce){ return static_cast<BYTE>(promo) << (pce ? 5 : 2); }
 
-    // Piece movement
-    int src[]{p.piece.rank, p.piece.file};
-    switch(p.piece.type){
-        case king: this->king_ply(src, p.dst); break;
-        case queen: this->encode_queen_rookmove(src, p.dst); this->queen_ply(src, p.dst); break;
-        case rook: this->rook_ply(src, p.dst); break;
-        case bishop: this->bishop_ply(src, p.dst); break;
-        case knight: this->knight_ply(src, p.dst); break;
-        case pawn: this->encode_pawn_promotion(p.promo); this->pawn_ply(src, p.dst); break;
-    }
+template<typename T>
+BYTE Encoder::encode_queen_axis(const coords<T>& src, const coords<T>& dst){ return ((src[0]==dst[0]) || (src[1]==dst[1])) ? QAROOK_ : QABISHOP_; }
 
-    this->set();
-    *dst = this->cply;
-}
-
-/*
-    PRIVATE FUNCS - MODIFY MEMBERS
-*/
-
-void Encoder::encode_name(pname p){ this->encode_name(p, &(this->name)); }
-
-void Encoder::encode_queen_rookmove(int *from, int *to){ this->encode_queen_rookmove(from, to, &(this->name)); }
-
-void Encoder::encode_pawn_promotion(ptype promo){ this->encode_pawn_promotion(promo, &(this->name)); }
-
-void Encoder::encode_capture(bool cap){ this->encode_capture(cap, &(this->action)); }
-
-void Encoder::encode_check(bool chk){ this->encode_check(chk, &(this->action)); }
-
-void Encoder::encode_mate(bool mte){ this->encode_mate(mte, &(this->action)); }
-
-void Encoder::pawn_ply(int *from, int *to){ this->pawn_ply(from, to, &(this->action), this->action & 1<<5); }
-
-void Encoder::knight_ply(int *from, int *to){ this->knight_ply(from, to, &(this->action)); }
-
-void Encoder::bishop_ply(int *from, int *to){ this->bishop_ply(from, to, &(this->action)); }
-
-void Encoder::rook_ply(int *from, int *to){ this->rook_ply(from, to, &(this->action)); }
-
-void Encoder::queen_ply(int *from, int *to){ this->queen_ply(from, to, &(this->action)); }
-
-void Encoder::king_ply(int *from, int *to){ this->king_ply(from, to, &(this->action)); }
-
-
-/*
-    PUBLIC FUNCS - MODIFY PROVIDED ADDRESSES
-*/
-
-void Encoder::encode_name(pname p, BYTE *name){ *name = static_cast<BYTE>(p); }
-
-void Encoder::encode_queen_rookmove(int *from, int *to, BYTE *name){ *name |= ((from[0]==to[0]) | (from[1]==to[1])) ? 1<<5 : 0; }
-
-void Encoder::encode_pawn_promotion(ptype promo, BYTE *name){ *name |= static_cast<BYTE>(promo)<<5; }
-
-void Encoder::encode_capture(bool cap, BYTE *act){ *act |= cap ? 1<<5 : 0; }
-
-void Encoder::encode_check(bool chk, BYTE *act){ *act |= chk ? 1<<6 : 0; }
-
-void Encoder::encode_mate(bool mte, BYTE *act){ *act |= mte ? 1<<7 : 0; }
-
-void Encoder::pawn_ply(int *from, int *to, BYTE *act, bool cap){
-    // 00x00cmm
-    int delta[]{to[0] - from[0], to[1] - from[1]};
-    *act |= (delta[0] < 0) ? 4 : 0;
-
-    if(cap){ *act |= (delta[1]>0) ? 1 : 0; }
-    else{ *act |= abs(delta[0])==2 ? 3 : 2; }
+template<typename T>
+BYTE Encoder::pawn_action(const coords<T>& src, const coords<T>& dst, bool cap, ptype promo){
+    // 000ppp<rr/ff>
+    BYTE res;
+    if(cap)  { res = dst[1] > src[1] ? 2 : 1; }
+    else     { res = abs(dst[0]-src[0]); }
+    if(promo){ res |= this->encode_pawn_promotion(promo, false); }
+    return res;
 };
 
-void Encoder::knight_ply(int *from, int *to, BYTE *act){
-    // 00000odt
-    int delta[]{to[0] - from[0], to[1] - from[1]};
-    bool r2 = abs(delta[0]) == 2,
-         rneg = delta[0] < 0,
-         fneg = delta[1] < 0;
+template<typename T>
+BYTE Encoder::knight_action(const coords<T>& src, const coords<T>& dst){
+    // 00000aot
+    bool f2 = abs(dst[1] - src[1]) == 2,
+         rneg = dst[0] < src[0],
+         fneg = dst[1] < src[1];
 
-    *act |= r2 ? 0 : 1<<1;
-    *act |= ((r2 & rneg) | (!r2 & fneg)) ? 1 : 0;
-    *act |= ((r2 & fneg) | (!r2 & rneg)) ? 1<<2 : 0;
+    return  (4 * f2) |
+            2*((f2 & rneg) || (!f2 & fneg)) |
+            1*((f2 & fneg) || (!f2 & rneg));
 };
 
-void Encoder::bishop_ply(int *from, int *to, BYTE *act){
+template<typename T>
+BYTE Encoder::bishop_action(const coords<T>& src, const coords<T>& dst){
     // 000admmm
-    int delta[]{to[0] - from[0], to[1] - from[1]};
+    coords<T> delta{dst[0] - src[0], dst[1] - src[1]};
 
-    *act |= ((delta[0] ^ delta[1]) < 0) ? 1<<4 : 0;
-    *act |= (delta[0] < 0) ? 1<<3 : 0;
-    *act |= abs(delta[0]);
+    return  16*(delta[0] != delta[1]) |
+            8*(delta[0] < 0) |
+            abs(delta[0]);
 };
 
-void Encoder::rook_ply(int *from, int *to, BYTE *act){
+template<typename T>
+BYTE Encoder::rook_action(const coords<T>& src, const coords<T>& dst){
     // 000admmm
-    int delta[]{to[0] - from[0], to[1] - from[1]};
+    coords<T> delta{dst[0] - src[0], dst[1] - src[1]};
 
-    *act |= (!delta[1]) ? ((1<<4) | abs(delta[0])) : abs(delta[1]);
-    *act |= ((delta[0] < 0) | (delta[1] < 0)) ? 1<<3 : 0;
+    return  16 * !delta[1] |
+            8*((delta[0] | delta[1]) < 0) |
+            (abs(delta[0]) | abs(delta[1]));
 };
 
-void Encoder::queen_ply(int *from, int *to, BYTE *act){
+template<typename T>
+BYTE Encoder::queen_action(const coords<T>& src, const coords<T>& dst){
     // 000admmm
-    if((to[0]==from[0]) | (to[1]==from[1])){ this->rook_ply(from, to, act); }
-    else{ this->bishop_ply(from, to, act); }
+    if((dst[0]==src[0]) | (dst[1]==src[1])){ return this->rook_action(src, dst); }
+    else                                   { return this->bishop_action(src, dst); }
 };
 
-void Encoder::king_ply(int *from, int *to, BYTE *act){
+template<typename T>
+BYTE Encoder::king_action(const coords<T>& src, const coords<T>& dst){
     // 000cffrr
-    int delta[]{to[0] - from[0], to[1] - from[1]};
-    bool cas = abs(delta[1]) == 2;
-    bool fneg = delta[1] < 0;
+    coords<T> delta{dst[0] - src[0], dst[1] - src[1]};
 
-    *act |= (delta[0] < 0) ? 2 : delta[0];
-    *act |= (fneg ? 2 : delta[1])<<2;
-    *act |= cas ? (fneg ? 5<<2 : 1<<2) : 0;
+    return  16*(abs(delta[1]) == 2) |
+            (delta[1] ? (delta[1] > 0 ? 4 : 8) : 0) |
+            (delta[0] ? (delta[0] > 0 ? 1 : 2) : 0);
 };
-
-
-void Encoder::encode_header(std::vector<pgndict> &tags, std::map<int,str> &dst){
-
-    // First value ALWAYS
-    dst[0] = "\0";
-
-    std::unordered_set<str> vals;
-    int i = 1;
-
-    // Collect all strings from pgndicts into unorderd_set
-    for(std::vector<pgndict>::iterator it=tags.begin(); it!=tags.end(); ++it){
-        for(pgndict::iterator jt=(*it).begin(); jt!=(*it).end(); ++jt){
-            if(!(jt->second).compare("\0")){ continue; }
-            vals.insert(jt->second);
-        }
-    }
-
-    // Enumerate them
-    for(std::unordered_set<str>::iterator it=vals.begin(); it!=vals.end(); ++it){
-        dst[i] = *it;
-    }
-}
 
 
 #endif
