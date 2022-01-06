2b2b85803f4a5496a64286abc4c357a9a8568b83
James Korge <james@korgetech.com>
2022-01-06 16:09:08 -0500

    Removed old build scripts

--- build/win/engine.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-echo g++ -std=c++17 -O3 -mlzcnt -mbmi -mpopcnt -march=x86-64 -flto -fuse-linker-plugin -Llib/win -o bin/win/engine src/engine.cpp icon.res -lchess -lparsestream
-g++ -std=c++17 -O3 -mlzcnt -mbmi -mpopcnt -march=x86-64 -flto -fuse-linker-plugin -Llib/win -o bin/win/engine src/engine.cpp icon.res -lchess -lparsestream
